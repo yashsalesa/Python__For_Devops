689deec95c93cc151ea38c3045ab28a956ef3a82
yash <yash.salesa@kpoint.com>
2024-12-02 22:42:05 +0530

    Strip string program

---
+++ Practice_programs/strip-text.py
@@ -0,0 +1,6 @@
+#Program to strip extra spaces from string
+
+Str="                              Hello       world and universe                     "
+print("Original String:",Str)
+print("Stripped String:",Str.strip())
+
