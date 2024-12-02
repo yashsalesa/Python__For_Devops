c4bfaf49d16c0c335d41a4c9e5a94b4624b9ba91
yash <yash.salesa@kpoint.com>
2024-12-02 22:59:15 +0530

    Integer in python

---
+++ Practice_programs/int.py
@@ -0,0 +1,15 @@
+# Integer variables
+num1 = 10
+num2 = 3
+
+# Integer Division
+result1 = num1 // num2  #Here double slash is to get floored value (integer)
+print("Integer Division:", result1)
+
+# Modulus (Remainder)
+result2 = num1 % num2
+print("Modulus (Remainder):", result2)
+
+# Absolute Value (To convert negative value to a positive value)
+result3 = abs(-7)
+print("Absolute Value:", result3)
